0194688d77584e129773b2f05daa57dadf396044
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-06-03 09:52:19 +0200

    Corrected sorting of running SQL queries

--- src/app/controllers/application_status/running_sql_queries_controller.php
+++ src/app/controllers/application_status/running_sql_queries_controller.php
@@ -1,6 +1,6 @@
 <?php
 class RunningSqlQueriesController extends ApplicationController {
-	
+
 	function index(){
 		$this->page_title = "Running SQL Queries";
 		$this->tpl_data["running_queries"] = $this->_get_running_queries();
@@ -9,9 +9,9 @@ class RunningSqlQueriesController extends ApplicationController {
 	function _get_running_queries(){
 		$rows = $this->dbmole->selectRows("
 			SELECT pid, query_start, AGE(CLOCK_TIMESTAMP(), query_start) AS duration, query
-			FROM pg_stat_activity 
-			WHERE query != '<IDLE>' AND query!='<insufficient privilege>' AND query NOT ILIKE '%pg_stat_activity%' 
-			ORDER BY query_start DESC
+			FROM pg_stat_activity
+			WHERE query != '<IDLE>' AND query!='<insufficient privilege>' AND query NOT ILIKE '%pg_stat_activity%'
+			ORDER BY query_start
 		");
 		return $rows;
 	}
